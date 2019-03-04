22a5d53b39125711730769ea8b831d1194a01119
Dino Perez <dperezhincapie@gmail.com>
2019-03-04 17:34:38 -0500

    Estado inicial del proyecto

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+app.use(express.static(__dirname + "/dist"));
+
+const server = app.listen(3000, () => {
+  console.log("Servidor iniciado en el puerto " + server.address().port);
+});
