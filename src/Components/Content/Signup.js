a89266d07c82fecbd1e2583152a04b59e0a0c5e8
Dino Perez <dperezhincapie@gmail.com>
2019-03-06 01:55:43 -0500

    Sign's

--- src/Components/Content/Signup.js
+++ src/Components/Content/Signup.js
@@ -1,10 +1,75 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 class Signup extends Component {
   render() {
     return (
-      <section className="">
-        <h1>Holi soy Signup</h1>
-      </section>
+      <article className="signup ">
+        <form className="form">
+          <h2 className="title">Sign Up</h2>
+          <div>
+            <label for="name" className="label">
+              Name
+            </label>
+            <input
+              type="text"
+              className="input"
+              placeholder="Name"
+              id="name"
+              name="name"
+              title="Name"
+            />
+          </div>
+          <div>
+            <label for="username" className="label">
+              Email
+            </label>
+            <input
+              type="text"
+              className="input"
+              placeholder="Email"
+              id="email"
+              name="email"
+              title="Email"
+            />
+          </div>
+          <div>
+            <label for="password" className="label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="input"
+              placeholder="Password"
+              id="password"
+              name="password"
+              title="Password"
+            />
+          </div>
+          <div>
+            <label for="confirm-password" className="label">
+              Confirm password
+            </label>
+            <input
+              type="confirm-password"
+              className="input"
+              placeholder="Password"
+              id="confirm-password"
+              name="confirm-password"
+              title="Confirm password"
+            />
+          </div>
+          <div>
+            <button type="button" className="button button-primary ">
+              Sign Up
+            </button>
+          </div>
+          <div>
+            <a>
+              ¿Ya tienes cuenta? <Link to="/signin">Ingresar</Link>
+            </a>
+          </div>
+        </form>
+      </article>
     );
   }
 }
