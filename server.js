626051d75648e09b125c311555d2d54918ab5078
Dino Perez <dperezhincapie@gmail.com>
2019-03-07 15:03:13 -0500

    pedro

--- server.js
+++ server.js
@@ -2,6 +2,4 @@ const express = require("express");
 const app = express();
 app.use(express.static(__dirname + "/dist"));
 
-const server = app.listen(3000, () => {
-  console.log("Servidor iniciado en el puerto " + server.address().port);
-});
+const server = app.listen(process.env.PORT || port);
