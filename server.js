8955e821a960c73d140cf0e64eb2f9037bfbb58c
Dino Perez <dperezhincapie@gmail.com>
2019-03-07 15:11:29 -0500

    pedro

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const express = require("express");
 const app = express();
 app.use(express.static(__dirname + "/dist"));
 
-const server = app.listen(process.env.PORT || port);
+const server = app.listen(process.env.PORT || 5000);
